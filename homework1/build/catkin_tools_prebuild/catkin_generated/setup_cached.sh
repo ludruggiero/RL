955a253a6fdccd4e48ff0a453bdc99a39dbe82d2
ludruggiero <ludovica.ruggiero01@gmail.com>
2023-12-17 17:34:12 +0100

    logs

--- homework1/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ homework1/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ludovica/RL/homework1/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/ludovica/RL/homework1/build/catkin_tools_prebuild'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/ludovica/RL/homework1/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/ludovica/RL/homework1/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
